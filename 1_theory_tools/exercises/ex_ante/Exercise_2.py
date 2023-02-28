005586c72ee7a2f4384e6976d39bedb1c50f140d
Stefan Bak <stefanbakpedersen@gmail.com>
2023-02-28 20:52:01 +0100

    Opgave 2

--- 1_theory_tools/exercises/ex_ante/Exercise_2.py
+++ 1_theory_tools/exercises/ex_ante/Exercise_2.py
@@ -10,16 +10,19 @@ def solve_VFI(par):
     tol = par.tol #convergence tol. level
     it = 0  #iteration counter 
     V_now = np.zeros([par.W+1]) #arbitrary starting values
+    beta = par.beta
     
     while (max_iter>= it and tol<delta):
         it = it+1
         V_next = V_now.copy()
         for w in range(par.W+1):
             #Fill in
-            
-            
-            
-            
+            c = np.arange(w+1)
+            w_c = w - c  
+            V_guess = np.sqrt(c)+beta*V_next[w_c]
+            V_now[w] = np.amax(V_guess) #Find the maximum value
+            Cstar[w] = np.argmax(V_guess) 
+
         delta = np.amax(np.abs(V_now - V_next))
     
     class sol: pass
